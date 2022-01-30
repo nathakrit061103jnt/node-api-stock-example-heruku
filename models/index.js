99cf2a0a1357ea4a16c81441ea203f734a42403f
Nathakrit Tissatian <std60010912540@acc.msu.ac.th>
2022-01-30 18:01:44 +0700

    add

--- models/index.js
+++ models/index.js
@@ -1,13 +1,23 @@
 const dbConfig = require("../configs/db.config");
+const URI =
+  "postgres://fsjifywvfraors:db7c1a43e608aafa926a221f762755aede62a9e9192356a6352cdfc2c1b37f05@ec2-54-208-139-247.compute-1.amazonaws.com:5432/dehgbcc4cv2opo";
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   operatorsAliases: false,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle,
+//   },
+// });
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+const sequelize = new Sequelize(URI, {
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
