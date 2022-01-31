58f304c036e96a7006c9c86883f9ddd5f972be65
Nathakrit Tissatian <std60010912540@acc.msu.ac.th>
2022-01-31 12:30:22 +0700

    update

--- models/index.js
+++ models/index.js
@@ -22,7 +22,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connection has been established successfully. ");
+    console.log("Connection has been established successfully.");
   })
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
